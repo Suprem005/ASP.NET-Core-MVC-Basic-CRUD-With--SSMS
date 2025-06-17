c105430f9856c482fa3e6bb11f8c2f0908066033
Suprem005 <suprem.sthao7@gmail.com>
2025-06-17 12:14:10 +0545

    Slight Naming changes to navigation bar

--- MyApp/Controllers/ItemsController.cs
+++ MyApp/Controllers/ItemsController.cs
@@ -24,6 +24,53 @@ namespace MyApp.Controllers
             return View();
         }
         [HttpPost]
+        public async Task<IActionResult> Create([Bind("Id, Name, Price")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(item); // add item to the context
+                await _context.SaveChangesAsync(); // save changes to the database
+                return RedirectToAction("Index"); // redirect to index after saving
+            }
+            return View(item); // if model state is not valid, return the view with the item
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var item = await _context.Items.FirstOrDefaultAsync(x => x.Id == id); // find the item by id from the database
+            return View(item); // return the view with the item to edit
+        }
+        [HttpPost]
+
+        public async Task<IActionResult> Edit(int id, [Bind("Id, Name, Price")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Update(item);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(item);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var item = await _context.Items.FirstOrDefaultAsync(x=>x.Id == id);
+            return View(item); // return the view with the item to delete
+
+        }
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var item = await _context.Items.FindAsync(id);
+            if (item != null)
+            {
+                _context.Items.Remove(item);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction("Index");
+
+        }
 
         //public IActionResult Overview()
         //{
